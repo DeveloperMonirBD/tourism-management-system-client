49473906cb8246a659a639336657d2a80e690d0e
Monir <mrmonir0558@gmail.com>
2025-01-17 13:00:28 +0600

    delete card in the dashboard management stories section

--- src/pages/DashBoard/ManageStories.jsx
+++ src/pages/DashBoard/ManageStories.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import useAxiosSecure from '../../hook/useAxiosSecure';
 import { AuthContext } from '../../provider/AuthProvider';
 import Loading from '../Loading';
+import Swal from 'sweetalert2';
 // import useAxiosPublic from '../../hook/useAxiosPublic';
 
 const ManageStories = () => {
@@ -10,7 +11,7 @@ const ManageStories = () => {
     const axiosSecure = useAxiosSecure();
 
     // Fetch stories using tanstack query
-    const { data: stories = [] } = useQuery({
+    const { data: stories = [], refetch } = useQuery({
         queryKey: ['stories'],
         queryFn: async () => {
             const res = await axiosSecure.get(`/api/manageStories/${user.email}`);
@@ -18,9 +19,38 @@ const ManageStories = () => {
         }
     });
 
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then(result => {
+            if (result.isConfirmed) {  
+                axiosSecure.delete(`/api/stories/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                        Swal.fire({
+                            title: 'Deleted!',
+                            text: 'Your file has been deleted.',
+                            icon: 'success'
+                        });
+                    }
+                })
+
+
+            }
+        });
+    }
+
+
     // Null check for user
     if (!user) return <Loading />;
-
 
     return (
         <div>
@@ -41,10 +71,12 @@ const ManageStories = () => {
                                 </div>
                             </div>
                         </div>
+                        
                         <div className="mt-4 flex justify-between">
                             <button onClick={() => (window.location.href = `/edit-story/${story._id}`)} className="bg-blue-500 text-white px-4 py-2 rounded">
                                 Edit
                             </button>
+
                             <button onClick={() => handleDelete(story._id)} className="bg-red-500 text-white px-4 py-2 rounded">
                                 Delete
                             </button>
